4d5858e6063ca51980c259df564d890a893c47e3
Paul Milne <hallhill@gmail.com>
2017-01-10 06:49:33 +0000

    files added and main container file started

---
+++ client/src/components/FilmBox.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var MoreLink = require('./MoreLink.jsx');
+var ShowTimes = require('./ShowTimes.jsx');
+var FilmTable = require('./FilmTable.jsx');
+var FilmTimes = require('./FilmTimes.jsx');
